630d8aefd9e4c30c8e7953470c6572cc7efa561e
Harmeet <har900@ymail.com>
2017-10-27 01:32:28 +0000

    makemehapi

---
+++ mmh_hello_hapi/index.js
@@ -0,0 +1,18 @@
+let hapi = require('hapi');
+let server = new hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+        reply('Hello hapi');
+    }
+});
+server.start((err) => {
+    if (err) throw err;
+});
